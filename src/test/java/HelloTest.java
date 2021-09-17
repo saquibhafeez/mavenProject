896686a90b321e3e40d6a1d8a65ce39ea1a31a1a
Saquib <saquib1212@gmail.com>
2021-09-17 18:40:39 +0700

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -4,6 +4,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("Hi");
+        System.out.println("github is good");
+        System.out.println("doing more work");
     }
 
 }
